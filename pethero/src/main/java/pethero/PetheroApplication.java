ab43ce460619b2d2286989bd74bf043660b915d5
CatalinaML <listecatalina7@gmail.com>
2023-02-06 15:32:53 -0300

    pethero

---
+++ pethero/src/main/java/pethero/PetheroApplication.java
@@ -0,0 +1,13 @@
+package pethero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetheroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetheroApplication.class, args);
+	}
+
+}
---
+++ pethero/src/test/java/pethero/PetheroApplicationTests.java
@@ -0,0 +1,13 @@
+package pethero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetheroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
