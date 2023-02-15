57e67a9916f575727df9aa90730d6fcc028f2bcd
CatalinaML <listecatalina7@gmail.com>
2023-02-15 12:57:47 -0300

    Update PetDAO.java

--- pethero/src/main/java/pethero/dao/PetDAO.java
+++ pethero/src/main/java/pethero/dao/PetDAO.java
@@ -1,6 +1,7 @@
 package pethero.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pethero.domain.Pet;
 
 import java.util.List;
@@ -10,5 +11,8 @@ public interface PetDAO extends JpaRepository<Pet, Integer> {
 
     Optional<Pet> findByIdPet(int idPet);
     List<Pet> findByIdOwner(int id);
+
+    @Query("SELECT p FROM Pet p WHERE p.idOwner = ?1 AND p.petSize = ?2")
+    List<Pet> findBySize(int id, String size);
 
 }
