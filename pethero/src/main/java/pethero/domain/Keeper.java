b7de916eccc37a21cedb95862045eb454bcc88df
CatalinaML <listecatalina7@gmail.com>
2023-03-21 12:33:01 -0300

    acomodo codigo

--- pethero/src/main/java/pethero/domain/Keeper.java
+++ pethero/src/main/java/pethero/domain/Keeper.java
@@ -1,5 +1,7 @@
 package pethero.domain;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
@@ -8,6 +10,7 @@ import java.io.Serializable;
 import java.util.Date;
 
 @Entity
+@Data
 @Table(name = "keeper")
 public class Keeper extends User implements Serializable {
     private static final long serialVersionUID = 1L;
@@ -23,65 +26,4 @@ public class Keeper extends User implements Serializable {
     private String endTime;
     @NotEmpty
     private String petSize;
-
-    public Keeper(int idUser, String username, String password, String name, String lastname, String birthdate, String email, String phone, String city, String type, float remuneration, String startDate, String finishDate, String startTime, String endTime, String petSize) {
-        super(idUser, username, password, name, lastname, birthdate, email, phone, city, type);
-        this.remuneration = remuneration;
-        this.startDate = startDate;
-        this.finishDate = finishDate;
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.petSize = petSize;
-    }
-
-    public Keeper() {
-    }
-
-    public float getRemuneration() {
-        return remuneration;
-    }
-
-    public String getStartDate() {
-        return startDate;
-    }
-
-    public String getFinishDate() {
-        return finishDate;
-    }
-
-    public String getStartTime() {
-        return startTime;
-    }
-
-    public String getEndTime() {
-        return endTime;
-    }
-
-    public String getPetSize() {
-        return petSize;
-    }
-
-    public void setRemuneration(float remuneration) {
-        this.remuneration = remuneration;
-    }
-
-    public void setStartDate(String startDate) {
-        this.startDate = startDate;
-    }
-
-    public void setFinishDate(String finishDate) {
-        this.finishDate = finishDate;
-    }
-
-    public void setStartTime(String startTime) {
-        this.startTime = startTime;
-    }
-
-    public void setEndTime(String endTime) {
-        this.endTime = endTime;
-    }
-
-    public void setPetSize(String petSize) {
-        this.petSize = petSize;
-    }
 }
--- pethero/src/main/java/pethero/domain/User.java
+++ pethero/src/main/java/pethero/domain/User.java
@@ -6,8 +6,10 @@ import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.util.List;
 
 @Entity
+@Data
 @Inheritance(strategy = InheritanceType.JOINED)
 @Table(name = "user")
 public class User implements Serializable {
@@ -31,127 +33,4 @@ public class User implements Serializable {
     private String phone;
     private String city;
     private String type;
-
-    public int getIdUser() {
-        return idUser;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public String getBirthdate() {
-        return birthdate;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public User(int idUser, String username, String password, String name, String lastname, String birthdate, String email, String phone, String city, String type) {
-        this.idUser = idUser;
-        this.username = username;
-        this.password = password;
-        this.name = name;
-        this.lastname = lastname;
-        this.birthdate = birthdate;
-        this.email = email;
-        this.phone = phone;
-        this.city = city;
-        this.type = type;
-    }
-
-    public User(String username, String password, String name, String lastname, String birthdate, String email, String phone, String city, String type) {
-        this.username = username;
-        this.password = password;
-        this.name = name;
-        this.lastname = lastname;
-        this.birthdate = birthdate;
-        this.email = email;
-        this.phone = phone;
-        this.city = city;
-        this.type = type;
-    }
-
-    public User() {
-    }
-
-    public void setIdUser(int idUser) {
-        this.idUser = idUser;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public void setBirthdate(String birthdate) {
-        this.birthdate = birthdate;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "idUser=" + idUser +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", name='" + name + '\'' +
-                ", lastname='" + lastname + '\'' +
-                ", birthdate='" + birthdate + '\'' +
-                ", email='" + email + '\'' +
-                ", phone='" + phone + '\'' +
-                ", city='" + city + '\'' +
-                '}';
-    }
 }
